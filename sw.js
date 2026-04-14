137556cfd94033b04c51f6539b0979aa6a13f02a
djordjegia-creator <djordje.gia@gmail.com>
2026-04-14 14:21:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'logoped-firebase-v3';
+const CACHE = 'logoped-firebase-v5';
 const ASSETS = ['/', '/index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
